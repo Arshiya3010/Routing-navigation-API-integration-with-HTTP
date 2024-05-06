9e1fcf5922bb102f48cdfd8f463cbb8aaeb6d497
Arshiya3010 <23pds03@wcc.edu.in>
2024-05-06 16:00:21 +0530

    first commit

---
+++ src/app/department-list/department-list.component.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-department-list',
+  templateUrl: './department-list.component.html',
+  styleUrls: ['./department-list.component.css']
+})
+export class DepartmentListComponent {
+  public getJsonValue:any;
+  public postJsonValue:any;
+  
+
+constructor(private http:HttpClient){
+  
+}
+ngOnInit():void{
+  this.getMethod();
+  this.postMethod();
+
+}
+public getMethod(){
+  this.http.get('https://jsonplaceholder.typicode.com/posts/1').subscribe(
+    (data)=>{
+      console.log(data);
+      this.getJsonValue=data;
+    }
+  );
+}
+public postMethod(){
+  const header = new HttpHeaders({
+    contentType:'application/json'
+  })
+  let body={
+    title:'foo',
+    body:'bar',
+    userId:1,
+  };
+  this.http.post('https://jsonplaceholder.typicode.com/posts/1',body,{headers:header}).subscribe(
+    (data)=>{
+      console.log(data);
+      this.postJsonValue=data;
+    }
+  );
+}
+}
+
+
